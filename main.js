fc1e21e7d0f1c60501bce770b5747f41da5a1666
muathye <muath.ye@gmail.com>
2022-10-29 02:05:56 +0300

    this keyword

--- main.js
+++ main.js
@@ -21,3 +21,34 @@ let names = [];
 users.forEach(user => names.push(user.name));
 
 document.getElementById('output').innerHTML = names.join(', ');
+
+// callback function should not be arrow in this case because the 'this' object will equal to 'window'
+document.getElementById('btn').addEventListener('click', function() {
+    // 'this' is defined for block scope
+    console.log(this); // <button id="btn">Click Me</button>
+
+    // function getDetails() {
+    //     console.log(this); // undefined
+    //     return `Click button ${this.getAttribute('id')}`;
+    // }
+    
+    // arrow function passes parent block variables , in this case the 'this' object will be passed
+    const getDetails = () => {
+        console.log(this); // undefined
+        return `Click button ${this.getAttribute('id')}`;
+    }
+
+    document.getElementById('output').innerHTML = getDetails();
+});
+
+// callback function can be arrow function if you pass a variable to be the object for the block
+document.getElementById('btn').addEventListener('click', (event) => {
+    // 'this' will be in this case event.currentTarget
+    console.log(event.currentTarget); // <button id="btn">Click Me</button>
+    
+    // you don't need block and return if you will return one line
+    const getDetails = () => `Click button ${event.currentTarget.getAttribute('id')}`;
+
+    document.getElementById('output').innerHTML = getDetails();
+});
+
